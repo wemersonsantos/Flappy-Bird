35d3570883348084810242680ec63a005c7d39e2
wemersonsantos <wemersonsantoseng@gmail.com>
2021-08-25 16:16:42 -0300

    iniciando o game

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var sprites = new Image();
+sprites.src = './sprites/sprites.png';
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d');
---
+++ index.ts
@@ -0,0 +1,6 @@
+const sprites = new Image();
+sprites.src = './sprites/sprites.png'
+
+
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const context = canvas.getContext('2d');
